158cd9cf96caeaaf6ff4141e7558834545adbbb5
bee <rkddbtjd123@gmail.com>
2021-10-08 16:47:09 +0900

    test: Add LotteryPredictions test cases

--- src/test/kotlin/programmers/level1/LotteryPredictionsTest.kt
+++ src/test/kotlin/programmers/level1/LotteryPredictionsTest.kt
@@ -8,7 +8,7 @@ internal class LotteryPredictionsTest {
     private val lotteryPredictions = LotteryPredictions()
 
     @Test
-    fun solution() {
+    fun test_1() {
         // given
         val lotto = intArrayOf(44, 1, 0, 0, 31, 25)
         val win = intArrayOf(31, 10, 45, 1, 6, 19)
@@ -18,6 +18,34 @@ internal class LotteryPredictionsTest {
 
         // then
         assertThat(result).isEqualTo(intArrayOf(3, 5))
+
+    }
+
+    @Test
+    fun test_2() {
+        // given
+        val lotto = intArrayOf(0, 0, 0, 0, 0, 0)
+        val win = intArrayOf(38, 19, 20, 40, 15, 25)
+
+        // when
+        val result = lotteryPredictions.solution(lotto, win)
+
+        // then
+        assertThat(result).isEqualTo(intArrayOf(1, 6))
+
+    }
+
+    @Test
+    fun test_3() {
+        // given
+        val lotto = intArrayOf(45, 4, 35, 20, 3, 9)
+        val win = intArrayOf(20, 9, 3, 45, 4, 35)
+
+        // when
+        val result = lotteryPredictions.solution(lotto, win)
+
+        // then
+        assertThat(result).isEqualTo(intArrayOf(1, 1))
 
     }
 }
