c7b96d8f37f6339693ff38bfbe6fa323a856b84f
bee <rkddbtjd123@gmail.com>
2021-10-14 23:14:08 +0900

    feat: Init FunctionDevelopment

---
+++ src/main/kotlin/programmers/level2/FunctionDevelopment.kt
@@ -0,0 +1,10 @@
+package programmers.level2
+
+class FunctionDevelopment {
+
+    fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+        var answer = intArrayOf()
+        return answer
+    }
+
+}
