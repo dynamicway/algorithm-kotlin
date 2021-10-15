2257fd728a791d349df8078b77f00b4dd2266048
bee <rkddbtjd123@gmail.com>
2021-10-15 14:35:02 +0900

    feat: Add FunctionDevelopment logic

--- src/main/kotlin/programmers/level2/FunctionDevelopment.kt
+++ src/main/kotlin/programmers/level2/FunctionDevelopment.kt
@@ -1,10 +1,39 @@
 package programmers.level2
 
+import java.util.*
+
 class FunctionDevelopment {
 
     fun solution(progresses: IntArray, speeds: IntArray): IntArray {
-        var answer = intArrayOf()
-        return answer
+        val queue = LinkedList<Pair>()
+        val size = progresses.size
+        val answer: LinkedList<Int> = LinkedList()
+        val erase = Pair(0, 0)
+        (0 until size).forEach { queue.add(Pair(progresses[it], speeds[it])) }
+        while (queue.isNotEmpty()) {
+            queue.forEach {
+                it.first += it.second
+            }
+            var count = 0
+            for (i in 0 until queue.size) {
+                if (queue[i].first > 99) {
+                    queue[i] = erase
+                    count++
+                } else break
+            }
+            if (count != 0) {
+                answer.add(count)
+                count = 0
+            }
+            queue.removeAll { it == erase }
+        }
+
+        return answer.toIntArray()
     }
+
+    private class Pair(
+        var first: Int,
+        var second: Int
+    )
 
 }
--- src/test/kotlin/programmers/level2/FunctionDevelopmentTest.kt
+++ src/test/kotlin/programmers/level2/FunctionDevelopmentTest.kt
@@ -1,5 +1,30 @@
 package programmers.level2
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
-internal class FunctionDevelopmentTest
+internal class FunctionDevelopmentTest {
+
+    private val functionDevelopment = FunctionDevelopment()
+
+    @ParameterizedTest
+    @MethodSource("solution_src")
+    fun solution(src: Triple<IntArray, IntArray, IntArray>) {
+        // when
+        val solution = functionDevelopment.solution(src.first, src.second)
+
+        // then
+        assertThat(solution).isEqualTo(src.third)
+    }
+
+    companion object {
+        @JvmStatic
+        fun solution_src(): List<Triple<IntArray, IntArray, IntArray>> {
+            return listOf(
+                Triple(intArrayOf(93, 30, 55), intArrayOf(1, 30, 5), intArrayOf(2, 1)),
+                Triple(intArrayOf(95, 90, 99, 99, 80, 99), intArrayOf(1, 1, 1, 1, 1, 1), intArrayOf(1, 3, 2))
+            )
+        }
+    }
+}
