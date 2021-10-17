3cac8edab30900dafd9521b40b6374c3dfd46f40
bee <rkddbtjd123@gmail.com>
2021-10-18 02:21:13 +0900

    feat: Add Camouflage logic

--- src/main/kotlin/programmers/level2/Camouflage.kt
+++ src/main/kotlin/programmers/level2/Camouflage.kt
@@ -2,7 +2,10 @@ package programmers.level2
 
 class Camouflage {
     fun solution(clothes: Array<Array<String>>): Int {
-        var answer = 0
-        return answer
+        return clothes.groupBy { it[1] }
+            .values
+            .map { it.size + 1 }
+            .reduce { acc, i -> acc * i }
+            .minus(1)
     }
 }
