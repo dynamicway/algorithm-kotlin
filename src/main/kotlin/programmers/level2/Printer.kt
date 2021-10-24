fecb4fae96a779d8cbe4daa624c5306e9fc1d6fd
bee <rkddbtjd123@gmail.com>
2021-10-25 00:32:44 +0900

    feat: init programmers printer

---
+++ src/main/kotlin/programmers/level2/Printer.kt
@@ -0,0 +1,8 @@
+package programmers.level2
+
+class Printer {
+    fun solution(priorities: IntArray, location: Int): Int {
+        var answer = 0
+        return answer
+    }
+}
---
+++ src/test/kotlin/programmers/level2/PrinterTest.kt
@@ -0,0 +1,41 @@
+package programmers.level2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class PrinterTest {
+
+    private val printer = Printer()
+
+
+    @ParameterizedTest
+    @MethodSource("test_src")
+    fun test(givenSrc: Triple<IntArray, Int, Int>) {
+        // when
+        val result = printer.solution(givenSrc.first, givenSrc.second)
+
+        // then
+        assertThat(result).isEqualTo(givenSrc.third)
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun test_src(): List<Triple<IntArray, Int, Int>> {
+            return listOf(
+                Triple(
+                    intArrayOf(2, 1, 3, 2),
+                    2,
+                    1
+                ),
+                Triple(
+                    intArrayOf(1, 1, 9, 1, 1, 1),
+                    0,
+                    5
+                )
+            )
+        }
+    }
+
+}
