2126462b59c6e0e3a86eb84b000dccd94f5f5e5e
bee <rkddbtjd123@gmail.com>
2021-10-26 00:29:46 +0900

    프로그래머스 2단계 가장 큰 수 초기화

---
+++ src/main/kotlin/programmers/level2/TheGreatestNumber.kt
@@ -0,0 +1,8 @@
+package programmers.level2
+
+class TheGreatestNumber {
+    fun solution(numbers: IntArray): String {
+        var answer = ""
+        return answer
+    }
+}
---
+++ src/test/kotlin/programmers/level2/TheGreatestNumberTest.kt
@@ -0,0 +1,35 @@
+package programmers.level2
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class TheGreatestNumberTest {
+
+    private val theGreatestNumber = TheGreatestNumber()
+
+    @ParameterizedTest
+    @MethodSource("test_src")
+    fun test(src: Pair<IntArray, String>) {
+        // when
+        val result = theGreatestNumber.solution(src.first)
+
+        // then
+        Assertions.assertThat(result).isEqualTo(src.second)
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun test_src(): List<Pair<IntArray, String>> = listOf(
+            Pair(
+                intArrayOf(6, 10, 2),
+                "6210"
+            ),
+            Pair(
+                intArrayOf(3, 30, 34, 5, 9),
+                "9534330"
+            )
+        )
+    }
+}
