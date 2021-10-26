38143a3d28e6b04b6f91852cc72efaa57a32555d
bee <rkddbtjd123@gmail.com>
2021-10-26 22:33:52 +0900

    프로그래머스 2단계 가장 큰 수 로직 수정

--- src/main/kotlin/programmers/level2/TheGreatestNumber.kt
+++ src/main/kotlin/programmers/level2/TheGreatestNumber.kt
@@ -4,9 +4,7 @@ import java.util.*
 
 class TheGreatestNumber {
     fun solution(numbers: IntArray): String {
-        val answer = LinkedList(numbers.map { it.toString() }).sortedWith(
-            kotlin.Comparator { s1, s2 -> (s2 + s1).compareTo(s1 + s2) }
-        ).joinToString(separator = "")
+        val answer = LinkedList(numbers.map { it.toString() }).sortedWith { s1, s2 -> (s2 + s1).compareTo(s1 + s2) }.joinToString(separator = "")
 
         if (answer[0] == '0') return "0"
         return answer
