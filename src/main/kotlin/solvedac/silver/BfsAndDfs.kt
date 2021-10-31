bb67141812b59c26e9be3f3c858f967190b71285
bee <rkddbtjd123@gmail.com>
2021-10-31 16:59:36 +0900

    solved.ac BfsAndDfs 초기화

---
+++ src/main/kotlin/solvedac/silver/BfsAndDfs.kt
@@ -0,0 +1,9 @@
+package solvedac.silver
+
+class BfsAndDfs {
+
+    fun solution(info: Triple<Int, Int, Int>, nodes: List<Pair<Int, Int>>): Pair<List<Int>, List<Int>> {
+        return Pair(listOf(), listOf())
+    }
+
+}
---
+++ src/test/kotlin/solvedac/silver/BfsAndDfsTest.kt
@@ -0,0 +1,31 @@
+package solvedac.silver
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class BfsAndDfsTest {
+
+    private val bfsAndDfs = BfsAndDfs()
+
+    @Test
+    fun test_1() {
+        // given
+        val info = Triple(4, 5, 1)
+        val nodes = listOf(
+            Pair(1, 2),
+            Pair(1, 3),
+            Pair(1, 4),
+            Pair(2, 4),
+            Pair(3, 4)
+        )
+
+        // when
+        val result = bfsAndDfs.solution(info, nodes)
+
+        // then
+        assertThat(result.first).isEqualTo(listOf(1, 2, 4, 3))
+        assertThat(result.second).isEqualTo(listOf(1, 2, 3, 4))
+
+    }
+
+}
