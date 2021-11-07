0a895ae97264a4d1c3a88a75085e87cbae0ae211
bee <rkddbtjd123@gmail.com>
2021-11-07 16:52:35 +0900

    test: 점프왕 젤리 테스트 추가

--- src/test/kotlin/solvedac/silver/JumpKingJellyTest.kt
+++ src/test/kotlin/solvedac/silver/JumpKingJellyTest.kt
@@ -32,4 +32,57 @@ internal class JumpKingJellyTest {
 
         assertThat(actualResult).isEqualTo("Hing")
     }
+
+    @Test
+    fun test_3() {
+        val givenSquare = listOf(
+            listOf(10, 10, 10),
+            listOf(1, 1, 1),
+            listOf(1, 1, -1)
+        )
+
+        val actualResult = jumpKingJelly.solution(givenSquare)
+
+        assertThat(actualResult).isEqualTo("Hing")
+    }
+
+    @Test
+    fun test_4() {
+        val givenSquare = listOf(
+            listOf(1, 1, 1),
+            listOf(1, 1, 1),
+            listOf(1, 1, -1)
+        )
+
+        val actualResult = jumpKingJelly.solution(givenSquare)
+
+        assertThat(actualResult).isEqualTo("HaruHaru")
+    }
+
+    @Test
+    fun test_5() {
+        val givenSquare = listOf(
+            listOf(1, 1, 1),
+            listOf(1, 1, 0),
+            listOf(0, 0, -1)
+        )
+
+        val actualResult = jumpKingJelly.solution(givenSquare)
+
+        assertThat(actualResult).isEqualTo("Hing")
+    }
+
+    @Test
+    fun test_6() {
+        val givenSquare = listOf(
+            listOf(0, 0, 0),
+            listOf(0, 0, 0),
+            listOf(0, 0, -1),
+        )
+
+        val actualResult = jumpKingJelly.solution(givenSquare)
+
+        assertThat(actualResult).isEqualTo("Hing")
+    }
+
 }
