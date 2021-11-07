1508b402b95a258632ba81c143084d1e751cc975
bee <rkddbtjd123@gmail.com>
2021-11-07 20:29:31 +0900

    feat: 바닥장식 초기화

---
+++ src/main/kotlin/solvedac/silver/FloorDecoration.kt
@@ -0,0 +1,6 @@
+package solvedac.silver
+
+// https://www.acmicpc.net/problem/1388
+class FloorDecoration {
+
+}
---
+++ src/test/kotlin/solvedac/silver/FloorDecorationTest.kt
@@ -0,0 +1,12 @@
+package solvedac.silver
+
+import org.junit.jupiter.api.Test
+
+internal class FloorDecorationTest {
+    private val floorDecoration = FloorDecoration()
+
+    @Test
+    fun test_1() {
+
+    }
+}
