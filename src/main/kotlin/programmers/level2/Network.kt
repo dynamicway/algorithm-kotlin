2dfd26079ee97552162e51129fe060eb0c6957fe
bee <rkddbtjd123@gmail.com>
2022-01-04 12:05:46 +0900

    feat: Network 초기화

---
+++ src/main/kotlin/programmers/level2/Network.kt
@@ -0,0 +1,11 @@
+package programmers.level2
+
+class Network {
+    fun solution(n: Int, computers: Array<IntArray>): Int {
+        // 탐색할 곳이 이미 탐색됐다면 stack에 저장하지 않는다.
+        // 탐색할 곳을 stack에 저장한다
+        // stack에서 하나 pop해서 그래프 탐색
+        // stack이 비어있을 때 까지 반복
+        return 0;
+    }
+}
---
+++ src/test/kotlin/programmers/level2/NetworkTest.kt
@@ -0,0 +1,14 @@
+package programmers.level2
+
+import io.kotest.core.spec.style.FreeSpec
+
+class NetworkTest : FreeSpec() {
+
+    private val network: Network = Network()
+
+    init {
+        "test_1" {
+        }
+    }
+
+}
