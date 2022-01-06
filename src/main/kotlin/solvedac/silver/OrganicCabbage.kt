b9ca477822919b8cbdf3c88c16e300520ee3a24d
bee <rkddbtjd123@gmail.com>
2022-01-07 00:09:12 +0900

    feat: OrganicCabbage 초기화

---
+++ src/main/kotlin/solvedac/silver/OrganicCabbage.kt
@@ -0,0 +1,11 @@
+package solvedac.silver
+
+import java.util.*
+
+// https://www.acmicpc.net/problem/1012
+class OrganicCabbage(
+    private val cabbageLocations: LinkedList<Pair<Int, Int>>,
+    private val search: Queue<Pair<Int, Int>> = LinkedList()
+) {
+
+}
---
+++ src/test/kotlin/solvedac/silver/OrganicCabbageTest.kt
@@ -0,0 +1,14 @@
+package solvedac.silver
+
+import io.kotest.core.spec.style.FreeSpec
+
+class OrganicCabbageTest : FreeSpec({
+
+    "container" - {
+
+        "test_1" {
+
+        }
+
+    }
+})
