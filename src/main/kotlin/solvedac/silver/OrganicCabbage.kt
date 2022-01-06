8198d4a6c0d435b1a438de1c0aad1bd4daafd12a
bee <rkddbtjd123@gmail.com>
2022-01-07 00:30:20 +0900

    feat: OrganicCabbage 구현

--- src/main/kotlin/solvedac/silver/OrganicCabbage.kt
+++ src/main/kotlin/solvedac/silver/OrganicCabbage.kt
@@ -5,7 +5,24 @@ import java.util.*
 // https://www.acmicpc.net/problem/1012
 class OrganicCabbage(
     private val cabbageLocations: LinkedList<Pair<Int, Int>>,
-    private val search: Queue<Pair<Int, Int>> = LinkedList()
 ) {
+    private val search: LinkedList<Pair<Int, Int>> = LinkedList()
+
+    fun getNumberOfEarthWarm(): Int {
+        var numberOfEarthWarm = 0
+        while (cabbageLocations.isNotEmpty()) {
+            search.add(cabbageLocations.pop())
+            while (search.isNotEmpty()) {
+                val cabbageLocation = search.pop()
+                cabbageLocations.forEach {
+                    if (((it.first + 1 == cabbageLocation.first || it.first - 1 == cabbageLocation.first) && it.second == cabbageLocation.second) || ((it.second + 1 == cabbageLocation.second || it.second - 1 == cabbageLocation.second) && it.first == cabbageLocation.first))
+                        search.add(it)
+                }
+                cabbageLocations.remove(cabbageLocation)
+            }
+            numberOfEarthWarm++
+        }
+        return numberOfEarthWarm
+    }
 
 }
--- src/test/kotlin/solvedac/silver/OrganicCabbageTest.kt
+++ src/test/kotlin/solvedac/silver/OrganicCabbageTest.kt
@@ -1,13 +1,74 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import java.util.*
 
 class OrganicCabbageTest : FreeSpec({
 
     "container" - {
 
         "test_1" {
+            val givenCabbageLocations = LinkedList(
+                listOf(
+                    Pair(0, 2),
+                    Pair(1, 2),
+                    Pair(2, 2),
+                    Pair(3, 2),
+                    Pair(4, 2),
+                    Pair(4, 0)
+                )
+            )
 
+            val organicCabbage = OrganicCabbage(
+                givenCabbageLocations
+            )
+
+            organicCabbage.getNumberOfEarthWarm() shouldBe 2
+        }
+
+        "test_2" {
+            val givenCabbageLocations = LinkedList(
+                listOf(
+                    Pair(0, 0),
+                    Pair(1, 0),
+                    Pair(1, 1),
+                    Pair(4, 2),
+                    Pair(4, 3),
+                    Pair(4, 5),
+                    Pair(2, 4),
+                    Pair(3, 4),
+                    Pair(7, 4),
+                    Pair(8, 4),
+                    Pair(9, 4),
+                    Pair(7, 5),
+                    Pair(8, 5),
+                    Pair(9, 5),
+                    Pair(7, 6),
+                    Pair(8, 6),
+                    Pair(9, 6)
+                )
+            )
+
+            val organicCabbage = OrganicCabbage(
+                givenCabbageLocations
+            )
+
+            organicCabbage.getNumberOfEarthWarm() shouldBe 5
+        }
+
+        "test_3" {
+            val givenCabbageLocations = LinkedList(
+                listOf(
+                    Pair(5, 5),
+                )
+            )
+
+            val organicCabbage = OrganicCabbage(
+                givenCabbageLocations
+            )
+
+            organicCabbage.getNumberOfEarthWarm() shouldBe 1
         }
 
     }
