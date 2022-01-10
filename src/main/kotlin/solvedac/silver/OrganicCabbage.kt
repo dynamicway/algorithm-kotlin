1de98e5c69eed517e54d08ab594f7261c0c06ec0
bee <rkddbtjd123@gmail.com>
2022-01-10 13:04:21 +0900

    feat: OrganicCabbage 로직 최적화

--- src/main/kotlin/solvedac/silver/OrganicCabbage.kt
+++ src/main/kotlin/solvedac/silver/OrganicCabbage.kt
@@ -3,6 +3,22 @@ package solvedac.silver
 import java.util.*
 
 // https://www.acmicpc.net/problem/1012
+fun main() = with(System.`in`.bufferedReader()) {
+    val results = mutableListOf<Int>()
+    val numberOfTestCases = readLine().toInt()
+    repeat(numberOfTestCases) {
+        val numberOfOrganicCabbage = readLine().split(" ").map { it.toInt() }.last()
+        val cabbageLocations: MutableList<Pair<Int, Int>> = mutableListOf()
+        repeat(numberOfOrganicCabbage) {
+            val cabbageLocation = readLine().split(" ").map { it.toInt() }.toIntArray()
+            cabbageLocations.add(Pair(cabbageLocation.first(), cabbageLocation.last()))
+        }
+        val organicCabbage = OrganicCabbage(LinkedList(cabbageLocations))
+        results.add(organicCabbage.getNumberOfEarthWarm())
+    }
+    results.forEach { println(it) }
+}
+
 class OrganicCabbage(
     private val cabbageLocations: LinkedList<Pair<Int, Int>>,
 ) {
@@ -14,11 +30,14 @@ class OrganicCabbage(
             search.add(cabbageLocations.pop())
             while (search.isNotEmpty()) {
                 val cabbageLocation = search.pop()
-                cabbageLocations.forEach {
-                    if (((it.first + 1 == cabbageLocation.first || it.first - 1 == cabbageLocation.first) && it.second == cabbageLocation.second) || ((it.second + 1 == cabbageLocation.second || it.second - 1 == cabbageLocation.second) && it.first == cabbageLocation.first))
+                val cabbageLocationsIterator = cabbageLocations.iterator()
+                while (cabbageLocationsIterator.hasNext()) {
+                    val it = cabbageLocationsIterator.next()
+                    if (((it.first + 1 == cabbageLocation.first || it.first - 1 == cabbageLocation.first) && it.second == cabbageLocation.second) || ((it.second + 1 == cabbageLocation.second || it.second - 1 == cabbageLocation.second) && it.first == cabbageLocation.first)) {
+                        cabbageLocationsIterator.remove()
                         search.add(it)
+                    }
                 }
-                cabbageLocations.remove(cabbageLocation)
             }
             numberOfEarthWarm++
         }
--- src/test/kotlin/solvedac/silver/OrganicCabbageTest.kt
+++ src/test/kotlin/solvedac/silver/OrganicCabbageTest.kt
@@ -71,5 +71,54 @@ class OrganicCabbageTest : FreeSpec({
             organicCabbage.getNumberOfEarthWarm() shouldBe 1
         }
 
+        "test_4" {
+            val givenCabbageLocations = LinkedList(
+                listOf(
+                    Pair(0, 5),
+                    Pair(1, 5),
+                    Pair(2, 5),
+                    Pair(3, 5),
+                    Pair(4, 5),
+                    Pair(5, 5),
+                    Pair(0, 4),
+                    Pair(1, 4),
+                    Pair(2, 4),
+                    Pair(3, 4),
+                    Pair(4, 4),
+                    Pair(5, 4),
+                    Pair(0, 3),
+                    Pair(1, 3),
+                    Pair(2, 3),
+                    Pair(3, 3),
+                    Pair(4, 3),
+                    Pair(5, 3),
+                    Pair(0, 2),
+                    Pair(1, 2),
+                    Pair(2, 2),
+                    Pair(3, 2),
+                    Pair(4, 2),
+                    Pair(5, 2),
+                    Pair(0, 1),
+                    Pair(1, 1),
+                    Pair(2, 1),
+                    Pair(3, 1),
+                    Pair(4, 1),
+                    Pair(5, 1),
+                    Pair(0, 0),
+                    Pair(1, 0),
+                    Pair(2, 0),
+                    Pair(3, 0),
+                    Pair(4, 0),
+                    Pair(5, 0),
+                )
+            )
+
+            val organicCabbage = OrganicCabbage(
+                givenCabbageLocations
+            )
+
+            organicCabbage.getNumberOfEarthWarm() shouldBe 1
+        }
+
     }
 })
