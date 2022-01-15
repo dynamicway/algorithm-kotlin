c7eabe556bd37db3577157505e05369ed4158a06
bee <rkddbtjd123@gmail.com>
2022-01-16 02:04:40 +0900

    feat: CountOfConnectionComponent 구현

--- src/main/kotlin/solvedac/silver/CountOfConnectionComponent.kt
+++ src/main/kotlin/solvedac/silver/CountOfConnectionComponent.kt
@@ -1,4 +1,48 @@
 package solvedac.silver
 
-class CountOfConnectionComponent {
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val graphInfo = readLine().split(" ").map { it.toInt() }
+    val adjacencyList = Array<LinkedList<Int>>(graphInfo.first()){ LinkedList() }
+    repeat(graphInfo.last()) {
+        val edge = readLine().split(" ").map { it.toInt() }
+        adjacencyList[edge.first() - 1].add(edge.last())
+        adjacencyList[edge.last() - 1].add(edge.first())
+    }
+    println(CountOfConnectionComponent(adjacencyList).getCountOfConnectedComponents())
+}
+
+
+class CountOfConnectionComponent(
+    private val adjacencyList: Array<LinkedList<Int>>
+) {
+    fun getCountOfConnectedComponents(): Int {
+        var result = 0
+        val visited = Array(adjacencyList.size + 1){ false }
+        visited[0] = true
+        visited[1] = true
+        val search = ArrayDeque<Int>(adjacencyList.size)
+        search.add(1)
+        while (search.isNotEmpty()) {
+            val node = search.pop()
+            val adjacencyNodes = adjacencyList[node - 1]
+            adjacencyNodes.filter { !visited[it] }
+                .forEach {
+                    visited[it] = true
+                    search.add(it)
+                }
+            if (search.isEmpty()) {
+                visited.indexOfFirst { !it }.let {
+                    if (it != -1) {
+                        visited[it] = true
+                        search.add(it)
+                    }
+                }
+                result++
+            }
+        }
+        return result
+    }
+
 }
--- src/test/kotlin/solvedac/silver/CountOfConnectionComponentTest.kt
+++ src/test/kotlin/solvedac/silver/CountOfConnectionComponentTest.kt
@@ -1,12 +1,63 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import java.util.*
 
 class CountOfConnectionComponentTest : FreeSpec({
 
     "container" - {
+        "test1" {
+            val adjacencyList = arrayOf(
+                LinkedList(listOf(1, 2, 5)),
+                LinkedList(listOf(2, 5)),
+                LinkedList(listOf(3, 4)),
+                LinkedList(listOf(4, 6)),
+                LinkedList(listOf(5, 2, 1)),
+                LinkedList(listOf(6, 4))
+            )
+
+            CountOfConnectionComponent(adjacencyList).getCountOfConnectedComponents() shouldBe 2
+        }
+
+        "test2" {
+            val adjacencyList = arrayOf(
+                LinkedList(listOf(1, 2, 5)),
+                LinkedList(listOf(2, 3, 4, 5, 1)),
+                LinkedList(listOf(3, 2, 4)),
+                LinkedList(listOf(4, 2, 3, 5, 6)),
+                LinkedList(listOf(5, 1, 2, 4)),
+                LinkedList(listOf(6, 4))
+            )
+
+            CountOfConnectionComponent(adjacencyList).getCountOfConnectedComponents() shouldBe 1
+
+        }
+
+        "test3" {
+            val adjacencyList = arrayOf(
+                LinkedList(listOf(1, 2, 5)),
+                LinkedList(listOf(2, 1, 3)),
+                LinkedList(listOf(3, 2, 4)),
+                LinkedList(listOf(4, 3, 5)),
+                LinkedList(listOf(5, 1, 4))
+            )
+
+            CountOfConnectionComponent(adjacencyList).getCountOfConnectedComponents() shouldBe 1
+        }
+
+        "test4" {
+            val adjacencyList = arrayOf(
+                LinkedList(listOf(1, 2, 3, 4, 5)),
+                LinkedList(listOf(2, 1)),
+                LinkedList(listOf(3, 1)),
+                LinkedList(listOf(4, 1)),
+                LinkedList(listOf(5, 1))
+            )
+
+            CountOfConnectionComponent(adjacencyList).getCountOfConnectedComponents() shouldBe 1
+        }
+
     }
 
 })
