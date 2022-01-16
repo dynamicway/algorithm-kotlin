c24bff5805c2c4e52939fafc7ccc775c81b58573
bee <rkddbtjd123@gmail.com>
2022-01-16 16:05:33 +0900

    feat: FindTheParentsOfTree 구현

--- src/main/kotlin/solvedac/silver/FindTheParentsOfTree.kt
+++ src/main/kotlin/solvedac/silver/FindTheParentsOfTree.kt
@@ -1,5 +1,37 @@
 package solvedac.silver
 
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val numberOfNodes = readLine().toInt()
+    val adjacencyList = Array<LinkedList<Int>>(numberOfNodes) { LinkedList() }
+    repeat(numberOfNodes - 1) {
+        val edge = readLine().split(" ").map { it.toInt() }
+        if (adjacencyList[edge.first() - 1].isEmpty()) adjacencyList[edge.first() - 1].add(edge.first() - 1)
+        if (adjacencyList[edge.last() - 1].isEmpty()) adjacencyList[edge.last() - 1].add(edge.last() - 1)
+        adjacencyList[edge.first() - 1].add(edge.last())
+        adjacencyList[edge.last() - 1].add(edge.first())
+    }
+    findTheParentsOfTree(adjacencyList).forEach { println(it.second) }
+}
+
 // https://www.acmicpc.net/problem/11725
-class FindTheParentsOfTree {
+val findTheParentsOfTree: (Array<LinkedList<Int>>) -> TreeSet<Pair<Int, Int>> = {
+    val parents = TreeSet<Pair<Int, Int>>() { o1, o2 ->
+        o1.first - o2.first
+    }
+    val visited = BooleanArray(it.size)
+    val search = ArrayDeque<Int>(it.size)
+    search.add(1)
+    while (search.isNotEmpty()) {
+        val node = search.removeLast()
+        visited[node - 1] = true
+        for (i in 1 until it[node - 1].size) {
+            when(visited[it[node - 1][i] - 1]) {
+                false -> search.add(it[node - 1][i])
+                true -> parents.add(Pair(node, it[node - 1][i]))
+            }
+        }
+    }
+    parents
 }
--- src/test/kotlin/solvedac/silver/FindTheParentsOfTreeTest.kt
+++ src/test/kotlin/solvedac/silver/FindTheParentsOfTreeTest.kt
@@ -1,8 +1,93 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import java.util.*
 
 class FindTheParentsOfTreeTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val tree = arrayOf(
+                LinkedList(listOf(1, 4, 6)),
+                LinkedList(listOf(2, 4)),
+                LinkedList(listOf(3, 6, 5)),
+                LinkedList(listOf(4, 1, 2, 7)),
+                LinkedList(listOf(5, 3)),
+                LinkedList(listOf(6, 1, 3)),
+                LinkedList(listOf(7, 4))
+            )
+
+            findTheParentsOfTree(tree) shouldBe setOf(
+                Pair(2, 4),
+                Pair(3, 6),
+                Pair(4, 1),
+                Pair(5, 3),
+                Pair(6, 1),
+                Pair(7, 4),
+            )
+        }
+
+        "test02" {
+            val tree = arrayOf(
+                LinkedList(listOf(1, 2, 3)),
+                LinkedList(listOf(2, 1, 4)),
+                LinkedList(listOf(3, 1, 6, 5)),
+                LinkedList(listOf(4, 2, 7, 8)),
+                LinkedList(listOf(5, 3, 9, 10)),
+                LinkedList(listOf(6, 3, 11, 12)),
+                LinkedList(listOf(7, 4)),
+                LinkedList(listOf(8, 4)),
+                LinkedList(listOf(9, 5)),
+                LinkedList(listOf(10, 5)),
+                LinkedList(listOf(11, 6)),
+                LinkedList(listOf(12, 6))
+            )
+
+            findTheParentsOfTree(tree) shouldBe setOf(
+                Pair(2, 1),
+                Pair(3, 1),
+                Pair(4, 2),
+                Pair(5, 3),
+                Pair(6, 3),
+                Pair(7, 4),
+                Pair(8, 4),
+                Pair(9, 5),
+                Pair(10, 5),
+                Pair(11, 6),
+                Pair(12, 6)
+            )
+        }
+
+        "test03" {
+            val tree = arrayOf(
+                LinkedList(listOf(1, 2)),
+                LinkedList(listOf(2, 1))
+            )
+
+            findTheParentsOfTree(tree) shouldBe setOf(
+                Pair(2, 1)
+            )
+        }
+
+        "test04" {
+            val tree = arrayOf(
+                LinkedList(listOf(1, 2, 3, 4, 5)),
+                LinkedList(listOf(2, 1)),
+                LinkedList(listOf(3, 1)),
+                LinkedList(listOf(4, 1)),
+                LinkedList(listOf(5, 1))
+            )
+
+            findTheParentsOfTree(tree) shouldBe setOf(
+                Pair(2, 1),
+                Pair(3, 1),
+                Pair(4, 1),
+                Pair(5, 1)
+            )
+        }
+
+    }
 
 })
