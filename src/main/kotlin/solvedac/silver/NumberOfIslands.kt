7a7834869f4a15c5517aba42774599e8308039b1
bee <rkddbtjd123@gmail.com>
2022-01-16 14:17:20 +0900

    feat: NumberOfIslands 구현

--- src/main/kotlin/solvedac/silver/NumberOfIslands.kt
+++ src/main/kotlin/solvedac/silver/NumberOfIslands.kt
@@ -1,5 +1,46 @@
 package solvedac.silver
 
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val results = mutableListOf<Int>()
+    while (true) {
+        val mapInfo = readLine().split(" ").map { it.toInt() }
+        if (mapInfo.first() == 0 && mapInfo.last() == 0) break
+        val map = Array(mapInfo.last()) { IntArray(0) }
+        for (i in map.indices) {
+            map[i] = readLine().split(" ").map { it.toInt() }.toIntArray()
+        }
+        results.add(numberOfIslands(map))
+    }
+    results.forEach { println(it) }
+}
+
 //https://www.acmicpc.net/problem/4963
-class NumberOfIslands {
+fun numberOfIslands(map: Array<IntArray>): Int {
+    val search = LinkedList<Pair<Int, Int>>()
+    var result = 0
+    for (i in map.indices) {
+        for (j in map[i].indices) {
+            if (map[i][j] == 1) {
+                search.add(Pair(i, j))
+                while (search.isNotEmpty()) {
+                    val location = search.removeLast()
+                    val row = location.first
+                    val colum = location.second
+                    map[row][colum] = 0
+                    if (row != 0 && colum != 0 && map[row - 1][colum - 1] == 1) search.add(Pair(row - 1, colum - 1))
+                    if (row != 0 && map[row - 1][colum] == 1) search.add(Pair(row - 1, colum))
+                    if (row != 0 && colum < map[row].size - 1 && map[row - 1][colum + 1] == 1) search.add(Pair(row - 1, colum + 1))
+                    if (colum != 0 && map[row][colum - 1] == 1) search.add(Pair(row, colum - 1))
+                    if (colum < map[row].size - 1 && map[row][colum + 1] == 1) search.add(Pair(row, colum + 1))
+                    if (row < map.size - 1 && colum != 0 && map[row + 1][colum - 1] == 1) search.add(Pair(row + 1, colum - 1))
+                    if (row < map.size - 1&& map[row + 1][colum] == 1) search.add(Pair(row + 1, colum))
+                    if (row < map.size - 1&& colum < map[row].size - 1 && map[row + 1][colum + 1] == 1) search.add(Pair(row + 1, colum + 1))
+                }
+                result++
+            }
+        }
+    }
+    return result
 }
--- src/test/kotlin/solvedac/silver/NumberOfIslandsTest.kt
+++ src/test/kotlin/solvedac/silver/NumberOfIslandsTest.kt
@@ -1,8 +1,72 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class NumberOfIslandsTest : FreeSpec({
 
+    "container" - {
+
+        "test1" {
+            val map = arrayOf(
+                intArrayOf(0)
+            )
+
+            numberOfIslands(map) shouldBe 0
+        }
+
+        "test2" {
+            val map = arrayOf(
+                intArrayOf(1, 0),
+                intArrayOf(0, 1)
+            )
+
+            numberOfIslands(map) shouldBe 1
+        }
+
+        "test3" {
+            val map = arrayOf(
+                intArrayOf(1, 1, 1),
+                intArrayOf(1, 1, 1)
+            )
+
+            numberOfIslands(map) shouldBe 1
+        }
+
+        "test4" {
+            val map = arrayOf(
+                intArrayOf(1, 0, 1, 0, 0),
+                intArrayOf(1, 0, 0, 0, 0),
+                intArrayOf(1, 0, 1, 0, 1),
+                intArrayOf(1, 0, 0, 1, 0)
+            )
+
+            numberOfIslands(map) shouldBe 3
+        }
+
+        "test5" {
+            val map = arrayOf(
+                intArrayOf(1, 1, 1, 0, 1),
+                intArrayOf(1, 0, 1, 0, 1),
+                intArrayOf(1, 0, 1, 0, 1),
+                intArrayOf(1, 0, 1, 1, 1)
+            )
+
+            numberOfIslands(map) shouldBe 1
+        }
+
+        "test6" {
+            val map = arrayOf(
+                intArrayOf(1, 0, 1, 0, 1),
+                intArrayOf(0, 0, 0, 0, 0),
+                intArrayOf(1, 0, 1, 0, 1),
+                intArrayOf(0, 0, 0, 0, 0),
+                intArrayOf(1, 0, 1, 0, 1)
+            )
+
+            numberOfIslands(map) shouldBe 9
+        }
+
+    }
 
 })
