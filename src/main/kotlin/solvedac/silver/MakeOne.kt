8e7151c119605739a6754e802d0e76ed2ccb48bd
bee <rkddbtjd123@gmail.com>
2022-01-18 10:11:00 +0900

    feat: MakeOne 초기화

---
+++ src/main/kotlin/solvedac/silver/MakeOne.kt
@@ -0,0 +1,7 @@
+package solvedac.silver
+
+// https://www.acmicpc.net/problem/1463
+fun makeOne(value: Int): Int {
+    var result = 0
+    return result
+}
---
+++ src/test/kotlin/solvedac/silver/MakeOneKtTest.kt
@@ -0,0 +1,21 @@
+package solvedac.silver
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class MakeOneKtTest : FreeSpec({
+
+    "container" - {
+
+        "test1" {
+            makeOne(2) shouldBe 1
+        }
+
+        "test2" {
+            makeOne(10) shouldBe 3
+        }
+
+    }
+
+})
