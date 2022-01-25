c121c0c56be1ab41645e8ffe5573d3a484b02cdb
bee <rkddbtjd123@gmail.com>
2022-01-25 14:23:04 +0900

    feat: 가장 긴 증가하는 수열 구현

--- src/main/kotlin/solvedac/silver/LongestIncreasingSubSequence.kt
+++ src/main/kotlin/solvedac/silver/LongestIncreasingSubSequence.kt
@@ -1,6 +1,22 @@
 package solvedac.silver
 
+fun main() = with(System.`in`.bufferedReader()) {
+    readLine()
+    val sequence = readLine().split(" ").map { it.toInt() }.toIntArray()
+    println(longestIncreasingSubSequence(sequence))
+}
+
 // https://www.acmicpc.net/problem/11053
 fun longestIncreasingSubSequence(sequence: IntArray): Int {
-    return 0
+    val dp = IntArray(sequence.size) { 1 }
+    dp[sequence.size - 1] = 1
+    var max = 1
+    for (i in sequence.size - 2 downTo 0) {
+        for (j in i until sequence.size) {
+            if (sequence[i] < sequence[j])
+                dp[i] = maxOf(dp[i], dp[j] + 1)
+        }
+        max = maxOf(dp[i], max)
+    }
+    return max
 }
--- src/test/kotlin/solvedac/silver/LongestIncreasingSubSequenceTest.kt
+++ src/test/kotlin/solvedac/silver/LongestIncreasingSubSequenceTest.kt
@@ -1,10 +1,146 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class LongestIncreasingSubSequenceTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val sequence = intArrayOf(
+                10,
+                20,
+                10,
+                30,
+                20,
+                50
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 4
+        }
+
+        "test02" {
+            val sequence = intArrayOf(
+                4,
+                5,
+                6,
+                3,
+                8,
+                7,
+                9
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 5
+        }
+
+        "test03" {
+            val sequence = intArrayOf(
+                1,
+                1,
+                1,
+                1,
+                1,
+                1
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 1
+
+        }
+
+        "test04" {
+            val sequence = intArrayOf(
+                1,
+                2,
+                3,
+                4,
+                5,
+                6
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 6
+        }
+
+        "test05" {
+            val sequence = intArrayOf(
+                1
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 1
+        }
+
+        "test06" {
+            val sequence = intArrayOf(
+                5,
+                4,
+                3,
+                2,
+                1
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 1
+        }
+
+        "test07" {
+            val sequence = intArrayOf(
+                3,
+                5,
+                7,
+                9,
+                2,
+                1,
+                4,
+                8
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 4
+        }
+
+        "test08" {
+            val sequence = intArrayOf(
+                10,
+                20,
+                30,
+                15,
+                25,
+                50
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 4
+        }
+
+        "test09" {
+            val sequence = intArrayOf(
+                30, 10, 20, 50, 60, 70
+            )
+
+            longestIncreasingSubSequence(sequence) shouldBe 5
+        }
+
+        "test10" {
+            val sequence = intArrayOf(1, 6, 2, 4, 5, 3, 7)
+
+            longestIncreasingSubSequence(sequence) shouldBe 5
+        }
+
+        "test11" {
+            val sequence = intArrayOf(10, 21, 1, 10, 20, 30, 40, 50, 60)
+
+            longestIncreasingSubSequence(sequence) shouldBe 7
+        }
+
+        "test12" {
+            val sequence = intArrayOf(1, 3, 5, 2, 3, 4)
+
+            longestIncreasingSubSequence(sequence) shouldBe 4
+        }
+
+        "test13" {
+            val sequence = intArrayOf(1, 4, 5, 2, 3, 4, 5)
+
+            longestIncreasingSubSequence(sequence) shouldBe 5
+        }
+
+    }
 
 })
