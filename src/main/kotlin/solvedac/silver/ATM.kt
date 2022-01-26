2c92eba1a6df3a242b937981c0d49015ff54f69e
bee <rkddbtjd123@gmail.com>
2022-01-27 01:16:25 +0900

    ATM 구현

--- src/main/kotlin/solvedac/silver/ATM.kt
+++ src/main/kotlin/solvedac/silver/ATM.kt
@@ -1,6 +1,16 @@
 package solvedac.silver
 
+fun main() = with(System.`in`.bufferedReader()) {
+    readLine()
+    val costs = readLine().split(" ").map { it.toInt() }.toList().sorted()
+    println(atm(costs))
+}
+
 // https://www.acmicpc.net/problem/11399
-fun atm(people: List<Int>): Int {
-    return 0
+fun atm(costs: List<Int>): Int {
+    var result = 0
+    costs.indices.forEach {
+        result += (costs.size - it) * costs[it]
+    }
+    return result
 }
--- src/test/kotlin/solvedac/silver/ATMTest.kt
+++ src/test/kotlin/solvedac/silver/ATMTest.kt
@@ -1,8 +1,17 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class ATMTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val costs = arrayListOf(1, 2, 3, 3, 4)
+            atm(costs) shouldBe 32
+        }
+
+    }
 
 })
