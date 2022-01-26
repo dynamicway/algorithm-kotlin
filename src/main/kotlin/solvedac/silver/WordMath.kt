622e9759a58b8cd1e4cc4be625c825a9e06a3782
bee <rkddbtjd123@gmail.com>
2022-01-27 02:33:03 +0900

    단어수학 구현

--- src/main/kotlin/solvedac/silver/WordMath.kt
+++ src/main/kotlin/solvedac/silver/WordMath.kt
@@ -1,6 +1,47 @@
 package solvedac.silver
 
+import kotlin.math.pow
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val size = readLine().toInt()
+    val words = Array(size) { "" }
+    repeat(size) {
+        words[it] = readLine()
+    }
+    println(wordMath(words))
+}
+
 // https://www.acmicpc.net/problem/1339
-fun wordMath(words: Array<Array<String>>): Long {
-    return 0
+fun wordMath(words: Array<String>): Long {
+    val wordCount = mutableListOf<Alphabet>()
+    words.forEach { word ->
+        word.forEachIndexed { index, alphabet ->
+            val count = 10.0.pow(word.length - index - 1).toInt()
+            val let = wordCount.firstOrNull { it.alphabet == alphabet.toString() }?.let {
+                it.count += count
+            }
+            if (let == null)
+                wordCount.add(Alphabet(alphabet.toString(), count))
+        }
+    }
+
+    wordCount.sort()
+
+    var max = 0L
+    wordCount.forEachIndexed { index, alphabet ->
+        max += (9 - index) * alphabet.count
+    }
+
+    return max
+}
+
+class Alphabet(
+    val alphabet: String,
+    var count: Int
+): Comparable<Alphabet>{
+
+    override fun compareTo(other: Alphabet): Int {
+        return other.count - count
+    }
+
 }
--- src/test/kotlin/solvedac/silver/WordMathTest.kt
+++ src/test/kotlin/solvedac/silver/WordMathTest.kt
@@ -1,9 +1,56 @@
 package solvedac.silver
 
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-class WordMathTest : FunSpec({
+class WordMathTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val words = arrayOf(
+                "AAA",
+                "AAA"
+            )
+
+            wordMath(words) shouldBe 1998
+        }
+
+        "test02" {
+            val words = arrayOf(
+                "GCF",
+                "ACDEB"
+            )
+
+            wordMath(words) shouldBe 99437
+        }
+
+        "test03" {
+            val words = arrayOf(
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J"
+            )
+
+            wordMath(words) shouldBe 45
+        }
+
+        "test04" {
+            val words = arrayOf(
+                "AB",
+                "BA"
+            )
+
+            wordMath(words) shouldBe 187
+        }
+
+    }
 
 })
