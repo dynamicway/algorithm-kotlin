b7cfb7f5b2916a37ff82f224a768c47d95fbcbb6
bee <rkddbtjd123@gmail.com>
2022-01-27 01:27:03 +0900

    단어수학 초기화

---
+++ src/main/kotlin/solvedac/silver/WordMath.kt
@@ -0,0 +1,6 @@
+package solvedac.silver
+
+// https://www.acmicpc.net/problem/1339
+fun wordMath(words: Array<Array<String>>): Long {
+    return 0
+}
---
+++ src/test/kotlin/solvedac/silver/WordMathTest.kt
@@ -0,0 +1,9 @@
+package solvedac.silver
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class WordMathTest : FunSpec({
+
+
+})
