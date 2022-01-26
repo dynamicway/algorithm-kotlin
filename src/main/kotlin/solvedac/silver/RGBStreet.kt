1bdecf7f7f762577e327b6bbd1b2a901b9fb089d
bee <rkddbtjd123@gmail.com>
2022-01-27 00:36:58 +0900

    RGB거리 구현

--- src/main/kotlin/solvedac/silver/RGBStreet.kt
+++ src/main/kotlin/solvedac/silver/RGBStreet.kt
@@ -1,6 +1,27 @@
 package solvedac.silver
 
+import kotlin.math.min
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val costs = Array(readLine().toInt()) { readLine().split(" ").map { it.toInt() }.toIntArray() }
+    println(rgbStreet(costs))
+}
+
 // https://www.acmicpc.net/problem/1149
-fun rgbStreet(cost: Array<IntArray>): Int {
-    return 0
+fun rgbStreet(costs: Array<IntArray>): Int {
+    val red = IntArray(costs.size)
+    val green = IntArray(costs.size)
+    val blue = IntArray(costs.size)
+
+    red[0] = costs[0][0]
+    green[0] = costs[0][1]
+    blue[0] = costs[0][2]
+
+    for (i in 1 until costs.size) {
+        red[i] = min(green[i - 1], blue[i - 1]) + costs[i][0]
+        green[i] = min(red[i - 1], blue[i - 1]) + costs[i][1]
+        blue[i] = min(red[i - 1], green[i - 1]) + costs[i][2]
+    }
+
+    return minOf(red.last(), green.last(), blue.last())
 }
--- src/test/kotlin/solvedac/silver/RGBStreetTest.kt
+++ src/test/kotlin/solvedac/silver/RGBStreetTest.kt
@@ -1,8 +1,70 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class RGBStreetTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val costs = arrayOf(
+                intArrayOf(26, 40, 83),
+                intArrayOf(49, 60, 57),
+                intArrayOf(13, 89, 99)
+            )
+
+            rgbStreet(costs) shouldBe 96
+        }
+
+        "test02" {
+            val costs = arrayOf(
+                intArrayOf(1, 100, 100),
+                intArrayOf(100, 1, 100),
+                intArrayOf(100, 100, 1)
+            )
+
+            rgbStreet(costs) shouldBe 3
+        }
+
+        "test03" {
+            val costs = arrayOf(
+                intArrayOf(1, 100, 100),
+                intArrayOf(100, 100, 100),
+                intArrayOf(1, 100, 100)
+            )
+
+            rgbStreet(costs) shouldBe 102
+        }
+
+        "test04" {
+            val costs = arrayOf(
+                intArrayOf(30, 19, 5),
+                intArrayOf(64, 77, 64),
+                intArrayOf(15, 19, 97),
+                intArrayOf(4, 71, 57),
+                intArrayOf(90, 86, 84),
+                intArrayOf(93, 32, 91)
+            )
+
+            rgbStreet(costs) shouldBe 208
+        }
+
+        "test05" {
+            val costs = arrayOf(
+                intArrayOf(71, 39, 44),
+                intArrayOf(32, 83, 55),
+                intArrayOf(51, 37, 63),
+                intArrayOf(89, 29, 100),
+                intArrayOf(83, 58, 11),
+                intArrayOf(65, 13, 15),
+                intArrayOf(47, 25, 29),
+                intArrayOf(60, 66, 19)
+            )
+
+            rgbStreet(costs) shouldBe 253
+        }
+
+    }
 
 })
