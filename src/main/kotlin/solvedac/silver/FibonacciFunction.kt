290000b2173ab63a0ab63a193b74552abe5aff20
bee <rkddbtjd123@gmail.com>
2022-01-28 01:03:13 +0900

    피보나치 함수 구현

--- src/main/kotlin/solvedac/silver/FibonacciFunction.kt
+++ src/main/kotlin/solvedac/silver/FibonacciFunction.kt
@@ -1,6 +1,27 @@
 package solvedac.silver
 
+fun main() = with(System.`in`.bufferedReader()) {
+    val size = readLine().toInt()
+    val values = IntArray(size) { readLine().toInt() }
+    fibonacciFunction(values).forEach {
+        println("${it!!.first} ${it.second}")
+    }
+}
+
 // https://www.acmicpc.net/problem/1003
-fun fibonacciFunction(values: IntArray): Int {
-    return 0
+fun fibonacciFunction(values: IntArray): Array<Pair<Int, Int>?> {
+    val max = values.maxOrNull()!!
+    val dp = Array<Pair<Int, Int>?>(max + 1) { null }
+    dp[0] = Pair(1, 0)
+    if (max > 0)
+        dp[1] = Pair(0, 1)
+    for (i in 2 .. max) {
+        dp[i] = Pair(dp[i - 2]!!.first + dp[i - 1]!!.first, dp[i - 2]!!.second + dp[i - 1]!!.second)
+    }
+
+    val result = Array(values.size) { i ->
+        dp[values[i]]
+    }
+
+    return result
 }
--- src/test/kotlin/solvedac/silver/FibonacciFunctionTest.kt
+++ src/test/kotlin/solvedac/silver/FibonacciFunctionTest.kt
@@ -6,5 +6,52 @@ import io.kotest.matchers.shouldBe
 
 class FibonacciFunctionTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val values = intArrayOf(0, 1, 3)
+
+            fibonacciFunction(values) shouldBe arrayOf(
+                Pair(1, 0),
+                Pair(0, 1),
+                Pair(1, 2)
+            )
+        }
+
+        "test02" {
+            val values = intArrayOf(6, 22)
+
+            fibonacciFunction(values) shouldBe arrayOf(
+                Pair(5, 8),
+                Pair(10946, 17711)
+            )
+        }
+
+        "test03" {
+            val values = intArrayOf(0)
+
+            fibonacciFunction(values) shouldBe arrayOf(
+                Pair(1, 0)
+            )
+        }
+
+        "test04" {
+            val values = intArrayOf(1)
+
+            fibonacciFunction(values) shouldBe arrayOf(
+                Pair(0, 1)
+            )
+        }
+
+        "test05" {
+            val values = intArrayOf(3)
+
+            fibonacciFunction(values) shouldBe arrayOf(
+                Pair(1, 2)
+            )
+        }
+
+    }
+
 
 })
