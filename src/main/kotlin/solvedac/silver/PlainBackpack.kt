eacb32efa5c4a8a13c8247f8e709f32a0f07dd58
bee <rkddbtjd123@gmail.com>
2022-01-27 16:53:27 +0900

    평범한 배낭 구현

--- src/main/kotlin/solvedac/silver/PlainBackpack.kt
+++ src/main/kotlin/solvedac/silver/PlainBackpack.kt
@@ -1,5 +1,44 @@
 package solvedac.silver
 
-fun plainBackpack(items: Array<Pair<Int, Int>>): Int {
-    return 0
+// https://www.acmicpc.net/problem/12865
+fun main() = with(System.`in`.bufferedReader()) {
+    val info = readLine().split(" ").map { it.toInt() }
+    val dp = Array(info[0]) {
+        val itemInfo = readLine().split(" ").map { it.toInt() }
+        Item(itemInfo[0], itemInfo[1])
+    }
+
+    println(plainBackpack(dp, info[1]))
 }
+
+fun plainBackpack(items: Array<Item>, ability: Int): Int {
+
+    val dp = Array(items.size) { IntArray(ability + 1) }
+
+    dp.forEachIndexed { itemIndex, worthValues ->
+        worthValues.forEachIndexed { currentMaxWeight, worth ->
+            if (items[itemIndex].weight > currentMaxWeight) {
+                if (itemIndex == 0)
+                    dp[itemIndex][currentMaxWeight] = 0
+                else
+                    dp[itemIndex][currentMaxWeight] = dp[itemIndex - 1][currentMaxWeight]
+            } else {
+                if (itemIndex == 0)
+                    dp[itemIndex][currentMaxWeight] = items[itemIndex].worth
+                else
+                    dp[itemIndex][currentMaxWeight] = maxOf(
+                        dp[itemIndex - 1][currentMaxWeight],
+                        dp[itemIndex - 1][currentMaxWeight - items[itemIndex].weight] + items[itemIndex].worth
+                    )
+            }
+
+        }
+    }
+
+    return dp.last().last()
+}
+
+class Item(
+    val weight: Int,
+    val worth: Int
+)
--- src/test/kotlin/solvedac/silver/PlainBackpackTest.kt
+++ src/test/kotlin/solvedac/silver/PlainBackpackTest.kt
@@ -1,8 +1,35 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class PlainBackpackTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val items = arrayOf(
+                Item(6, 13),
+                Item(4, 8),
+                Item(3, 6),
+                Item(5, 12)
+            )
+
+            plainBackpack(items, 7) shouldBe 14
+        }
+
+        "test02" {
+            val items = arrayOf(
+                Item(3, 90),
+                Item(103, 89),
+                Item(2, 87),
+                Item(99, 86),
+                Item(98, 85)
+            )
+
+            plainBackpack(items, 104) shouldBe 263
+        }
+
+    }
 
 })
