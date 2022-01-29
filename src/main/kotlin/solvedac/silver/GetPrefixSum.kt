fb673b6c6ce34d23879f5f85081bb222b95351b6
bee <rkddbtjd123@gmail.com>
2022-01-29 23:42:27 +0900

    부분합 구하기 구현

--- src/main/kotlin/solvedac/silver/GetPrefixSum.kt
+++ src/main/kotlin/solvedac/silver/GetPrefixSum.kt
@@ -1,6 +1,39 @@
 package solvedac.silver
 
+fun main() = with(System.`in`.bufferedReader()) {
+    val info = readLine().split(" ").map { it.toInt() }
+    val map = ArrayList<List<Int>>(info[0])
+    repeat(info[0]) {
+        map.add(readLine().split(" ").map { it.toInt() })
+    }
+    val answers = ArrayList<List<Int>>(info[1])
+    repeat(info[1]) {
+        answers.add(readLine().split(" ").map { i -> i.toInt() })
+    }
+    getPrefixSum(map, answers).forEach {
+        println(it)
+    }
+}
+
+
 // https://www.acmicpc.net/problem/11660
-fun getPrefixSum(map: List<List<Int>>, answer: List<List<Int>>): Int {
-    return 0
+fun getPrefixSum(map: List<List<Int>>, answers: List<List<Int>>): IntArray {
+    val dp = List(map.size + 1) { IntArray(map[0].size + 1) }
+    for (row in 1 until map.size + 1) {
+        for (col in 1 until map[row - 1].size + 1) {
+            dp[row][col] = dp[row - 1][col] + dp[row][col - 1] - dp[row - 1][col - 1] + map[row - 1][col - 1]
+        }
+    }
+
+    val result = IntArray(answers.size)
+
+    answers.forEachIndexed { i, answer ->
+        val row1 = answer[0]
+        val col1 = answer[1]
+        val row2 = answer[2]
+        val col2 = answer[3]
+        result[i] = dp[row2][col2] - dp[row1 - 1][col2] - dp[row2][col1 - 1] + dp[row1 - 1][col1 - 1]
+    }
+
+    return result
 }
--- src/test/kotlin/solvedac/silver/GetPrefixSumTest.kt
+++ src/test/kotlin/solvedac/silver/GetPrefixSumTest.kt
@@ -1,10 +1,29 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class GetPrefixSumTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val map = listOf(
+                listOf(1, 2, 3, 4),
+                listOf(2, 3, 4, 5),
+                listOf(3, 4, 5, 6),
+                listOf(4, 5, 6, 7)
+            )
+
+            val answer = listOf(
+                listOf(2, 2, 3, 4),
+                listOf(3, 4, 3, 4),
+                listOf(1, 1, 4, 4)
+            )
+
+            getPrefixSum(map, answer) shouldBe listOf(27, 6, 64)
+        }
+
+    }
 
 })
