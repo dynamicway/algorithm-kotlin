51d2455235751a881114db444a0393848e6985c0
bee <rkddbtjd123@gmail.com>
2022-01-29 13:37:32 +0900

    부분합 구하기 초기화

---
+++ src/main/kotlin/solvedac/silver/GetPrefixSum.kt
@@ -0,0 +1,6 @@
+package solvedac.silver
+
+// https://www.acmicpc.net/problem/11660
+fun getPrefixSum(map: List<List<Int>>, answer: List<List<Int>>): Int {
+    return 0
+}
---
+++ src/test/kotlin/solvedac/silver/GetPrefixSumTest.kt
@@ -0,0 +1,10 @@
+package solvedac.silver
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class GetPrefixSumTest : FreeSpec({
+
+
+})
