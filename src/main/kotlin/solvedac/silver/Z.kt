b4a1b11cbb4bb7f53d27669ee1afe7544873bd21
bee <rkddbtjd123@gmail.com>
2022-02-09 12:36:16 +0900

    Z 구현

--- src/main/kotlin/solvedac/silver/Z.kt
+++ src/main/kotlin/solvedac/silver/Z.kt
@@ -1,6 +1,34 @@
 package solvedac.silver
 
-// https://www.acmicpc.net/problem/1074
-fun z(n: Int, row: Int, column: Int): Int {
-    return 0
+import kotlin.math.pow
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val input = readLine().split(" ").map { it.toInt() }.toList()
+    println(z(input[0], input[1], input[2]))
 }
+
+// https://www.acmicpc.net/problem/1074
+internal fun z(n: Int, r: Int, c: Int): Int {
+    var result = 0
+    var currentSize = n
+    var row = r + 1
+    var colum = c + 1
+    while (currentSize != 0) {
+        val length = 2.pow(--currentSize)
+        val area = length.pow(2)
+        if (length in colum until row) {
+            row -= length
+            result += 2 *area
+        } else if (length in row until colum) {
+            colum -= length
+            result += area
+        } else if (row > length) {
+            row -= length
+            colum -= length
+            result += 3 * area
+        }
+    }
+    return result
+}
+
+private fun Int.pow(n: Int) = this.toDouble().pow(n).toInt()
--- src/test/kotlin/solvedac/silver/ZTest.kt
+++ src/test/kotlin/solvedac/silver/ZTest.kt
@@ -1,10 +1,35 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class ZTest : FreeSpec({
 
     "container" - {
+
+        "test01" {
+            z(2, 3, 1) shouldBe 11
+        }
+
+        "test02" {
+            z(3, 7, 7) shouldBe 63
+        }
+
+        "test03" {
+            z(1, 0, 0) shouldBe 0
+        }
+
+        "test04" {
+            z(4, 7, 7) shouldBe 63
+        }
+
+        "test05" {
+            z(10, 511, 511) shouldBe 262143
+        }
+
+        "test06" {
+            z(10, 512, 512) shouldBe 786432
+        }
 
     }
 
