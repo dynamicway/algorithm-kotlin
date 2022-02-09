664ef5098316bb2cd7e03c653af75592bfc3e3fc
bee <rkddbtjd123@gmail.com>
2022-02-09 11:53:33 +0900

    Z 초기화

---
+++ src/main/kotlin/solvedac/silver/Z.kt
@@ -0,0 +1,6 @@
+package solvedac.silver
+
+// https://www.acmicpc.net/problem/1074
+fun z(n: Int, row: Int, column: Int): Int {
+    return 0
+}
---
+++ src/test/kotlin/solvedac/silver/ZTest.kt
@@ -0,0 +1,11 @@
+package solvedac.silver
+
+import io.kotest.core.spec.style.FreeSpec
+
+class ZTest : FreeSpec({
+
+    "container" - {
+
+    }
+
+})
