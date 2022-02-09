636f6e30bd2b5c0363abce496f867d3a3cf5fc7f
bee <rkddbtjd123@gmail.com>
2022-02-10 00:57:40 +0900

    포켓몬 마스터 구현

--- src/main/kotlin/solvedac/silver/ImPokemonMaster.kt
+++ src/main/kotlin/solvedac/silver/ImPokemonMaster.kt
@@ -1,6 +1,32 @@
 package solvedac.silver
 
-// https://www.acmicpc.net/problem/1620
-fun imPokemonMaster(pokeBook: List<Any>): List<Any> {
-    return listOf()
+fun main() = with(System.`in`.bufferedReader()) {
+    val input = readLine().split(" ").map { it.toInt() }.toList()
+    val pokeBook = ArrayList<String>()
+    repeat(input[0]) {
+        pokeBook.add(readLine())
+    }
+    val answers = ArrayList<String>()
+    repeat(input[1]) {
+        answers.add(readLine())
+    }
+    imPokemonMaster(pokeBook, answers).forEach { println(it) }
 }
+
+// https://www.acmicpc.net/problem/1620
+fun imPokemonMaster(pokeBook: List<String>, answers: List<String>): List<Any> {
+    val pokeBookHashMap = HashMap<String, Int>()
+    pokeBook.forEachIndexed { index, s ->
+        pokeBookHashMap[s] = index + 1
+    }
+    val result = mutableListOf<Any>()
+    answers.forEach { answer ->
+        when (answer.isInt()) {
+            false -> result.add(pokeBookHashMap[answer]!!)
+            true -> result.add(pokeBook[answer.toInt() - 1])
+        }
+    }
+    return result
+}
+
+private fun String.isInt() = this.contains("\\d".toRegex())
--- src/test/kotlin/solvedac/silver/ImPokemonMasterTest.kt
+++ src/test/kotlin/solvedac/silver/ImPokemonMasterTest.kt
@@ -1,8 +1,60 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class ImPokemonMasterTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            val pokeBook = listOf(
+                "Bulbasaur",
+                "Ivysaur",
+                "Venusaur",
+                "Charmander",
+                "Charmeleon",
+                "Charizard",
+                "Squirtle",
+                "Wartortle",
+                "Blastoise",
+                "Caterpie",
+                "Metapod",
+                "Butterfree",
+                "Weedle",
+                "Kakuna",
+                "Beedrill",
+                "Pidgey",
+                "Pidgeotto",
+                "Pidgeot",
+                "Rattata",
+                "Raticate",
+                "Spearow",
+                "Fearow",
+                "Ekans",
+                "Arbok",
+                "Pikachu",
+                "Raichu"
+            )
+
+            val answer = listOf(
+                "25",
+                "Raichu",
+                "3",
+                "Pidgey",
+                "Kakuna"
+            )
+
+            imPokemonMaster(pokeBook, answer) shouldBe listOf(
+                "Pikachu",
+                26,
+                "Venusaur",
+                16,
+                14
+            )
+
+        }
+
+    }
 
 })
