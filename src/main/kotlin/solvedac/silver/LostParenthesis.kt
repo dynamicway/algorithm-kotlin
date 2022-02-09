1f942f54e53702e8c89927eb7f7a53cf694ba49b
bee <rkddbtjd123@gmail.com>
2022-02-10 00:25:11 +0900

    잃어버린 괄호 구현

--- src/main/kotlin/solvedac/silver/LostParenthesis.kt
+++ src/main/kotlin/solvedac/silver/LostParenthesis.kt
@@ -1,5 +1,33 @@
 package solvedac.silver
 
-fun lostParenthesis(numbers: List<Int>): Int {
-    return 0
+// https://www.acmicpc.net/problem/1541
+fun main() = with(System.`in`.bufferedReader()) {
+    val src = readLine()
+    var start = 0
+    val numbers = mutableListOf<Int>()
+    for (index in 1 until src.length) {
+        if (src[index] == '+' || src[index] == '-') {
+            numbers.add(src.slice(start until index).toInt())
+            start = index
+        } else if (index == src.length - 1) {
+            if (start == 0) {
+                numbers.add(src.toInt())
+            } else if (src[start] == '+')
+                numbers.add(src.slice(start + 1..index).toInt())
+            else if (src[start] == '-')
+                numbers.add(-src.slice(start + 1..index).toInt())
+        }
+    }
+    if (src.length == 1)
+        numbers.add(src.toInt())
+    println(lostParenthesis(numbers))
+}
+
+fun lostParenthesis(numbers: MutableList<Int>): Int {
+    for (i in 1 until numbers.size) {
+        if (numbers[i - 1] < 0 && numbers[i] > 0) {
+            numbers[i] = -numbers[i]
+        }
+    }
+    return numbers.sum()
 }
--- src/test/kotlin/solvedac/silver/LostParenthesisTest.kt
+++ src/test/kotlin/solvedac/silver/LostParenthesisTest.kt
@@ -1,13 +1,22 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class LostParenthesisTest : FreeSpec({
 
     "container" - {
 
         "test01" {
+            lostParenthesis(mutableListOf(55, -50, 40)) shouldBe -35
+        }
 
+        "test02" {
+            lostParenthesis(mutableListOf(10, 20, 30, 40)) shouldBe 100
+        }
+
+        "test03" {
+            lostParenthesis(mutableListOf(9, -9)) shouldBe 0
         }
 
     }
