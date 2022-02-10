c00f943e9dabbaa6404989e8ac962b3ed417adc5
bee <rkddbtjd123@gmail.com>
2022-02-11 00:50:05 +0900

    리모컨 구현

--- src/main/kotlin/solvedac/gold/Remote.kt
+++ src/main/kotlin/solvedac/gold/Remote.kt
@@ -1,7 +1,35 @@
 package solvedac.gold
 
-import java.util.*
+import kotlin.math.absoluteValue
 
-fun getRemotePushCount(channel: Int, brokenButtons: TreeSet<Int>): Int {
-    return 0
+fun main() = with(System.`in`.bufferedReader()) {
+    val channel = readLine().toInt()
+    val brokenButtons = BooleanArray(10)
+    if (readLine() != "0") {
+        readLine().split(" ").map { it.toInt() }.forEach {
+            brokenButtons[it] = true
+        }
+        println(getRemotePushCount(channel, brokenButtons))
+    } else
+        println(getRemotePushCount(channel, brokenButtons))
+}
+
+// https://www.acmicpc.net/problem/1107
+fun getRemotePushCount(channel: Int, brokenButtons: BooleanArray): Int {
+    var result = (channel - 100).absoluteValue
+
+    for (i in 0 until 1000000) {
+        val number = i.toString()
+        var isBreak = false
+        for (element in number) {
+            if (brokenButtons[element.digitToInt()]) {
+                isBreak = true
+                break
+            }
+        }
+        if (!isBreak)
+            result = minOf(result, number.length + (channel - i).absoluteValue)
+    }
+
+    return result
 }
