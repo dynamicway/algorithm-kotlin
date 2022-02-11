50812e2edff149bd638a8a731bec65e670bc19b7
bee <rkddbtjd123@gmail.com>
2022-02-11 10:39:51 +0900

    팩토리얼 0의 개수 구현

--- src/main/kotlin/solvedac/silver/CountOfFactorialZero.kt
+++ src/main/kotlin/solvedac/silver/CountOfFactorialZero.kt
@@ -1,6 +1,22 @@
 package solvedac.silver
 
+import java.math.BigInteger
+
+fun main() = with(System.`in`.bufferedReader()) {
+    println(countOfFactorialZero(readLine().toInt()))
+}
+
 // https://www.acmicpc.net/problem/1676
 fun countOfFactorialZero(number: Int): Int {
-    return 0
+    var factorial = BigInteger.ONE
+    for (i in 1 .. number) {
+        factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+    }
+    var result = 0
+    val factorialString = factorial.toString()
+    for (i in factorialString.length - 1 downTo 0) {
+        if (factorialString[i] != '0') break
+        result++
+    }
+    return result
 }
--- src/test/kotlin/solvedac/silver/CountOfFactorialZeroTest.kt
+++ src/test/kotlin/solvedac/silver/CountOfFactorialZeroTest.kt
@@ -1,11 +1,31 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class CountOfFactorialZeroTest : FreeSpec({
 
     "container" - {
 
+        "test01" {
+            countOfFactorialZero(10) shouldBe 2
+        }
+
+        "test02" {
+            countOfFactorialZero(3) shouldBe 0
+        }
+
+        "test03" {
+            countOfFactorialZero(500) shouldBe 124
+        }
+
+        "test06" {
+            countOfFactorialZero(1) shouldBe 0
+        }
+
+        "test07" {
+            countOfFactorialZero(0) shouldBe 0
+        }
 
 
     }
