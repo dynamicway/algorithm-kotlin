0ba6507f05416be8c9c76c3943f5fd5527d464d0
bee <rkddbtjd123@gmail.com>
2022-02-11 17:47:01 +0900

    케빈베이컨 구현

--- src/main/kotlin/solvedac/silver/KevinBacon.kt
+++ src/main/kotlin/solvedac/silver/KevinBacon.kt
@@ -1,5 +1,46 @@
 package solvedac.silver
 
-fun kevinBacon(friends: Set<List<Int>>): Int {
-    return 0
+fun main() = with(System.`in`.bufferedReader()) {
+    val input = readLine().split(" ").map { it.toInt() }
+    val friends = Array(input[0]) { IntArray(input[0]) }
+    friends.forEachIndexed { index, ints ->
+        repeat(input[0]) { i ->
+            ints[i] = 5001
+            if (index == i)
+                ints[i] = 0
+        }
+
+    }
+    repeat(input[1]) {
+        val relationShip = readLine().split(" ").map { it.toInt() }
+        friends[relationShip[0] - 1][relationShip[1] - 1] = 1
+        friends[relationShip[1] - 1][relationShip[0] - 1] = 1
+    }
+    println(kevinBacon(friends))
+}
+
+// https://www.acmicpc.net/problem/1389
+fun kevinBacon(friends: Array<IntArray>): Int {
+    val SIZE = friends.size
+    for (k in 0 until SIZE) {
+        for (i in 0 until SIZE) {
+            if (i == k) continue
+            for (j in 0 until SIZE) {
+                if (j == k || i == j) continue
+                friends[i][j] = minOf(friends[i][j], friends[i][k] + friends[k][j])
+            }
+        }
+    }
+    var min = 5001
+    var kevenBacon = 101
+    friends.forEachIndexed { index, it ->
+        val sum = it.sum()
+        if (min > sum) {
+            min = sum
+            val node = index + 1
+            kevenBacon = node
+        }
+
+    }
+    return kevenBacon
 }
--- src/test/kotlin/solvedac/silver/KevinBaconTest.kt
+++ src/test/kotlin/solvedac/silver/KevinBaconTest.kt
@@ -1,12 +1,23 @@
 package solvedac.silver
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class KevinBaconTest : FreeSpec({
 
     "container" - {
 
+        "test01" {
+            val friends = arrayOf(
+                intArrayOf(0, 5001, 1, 1, 5001),
+                intArrayOf(5001, 0, 1, 5001, 5001),
+                intArrayOf(1, 1, 0, 1, 5001),
+                intArrayOf(1, 5001, 1, 0, 1),
+                intArrayOf(5001, 5001, 5001, 1, 0)
+            )
 
+            kevinBacon(friends) shouldBe 3
+        }
 
     }
 
