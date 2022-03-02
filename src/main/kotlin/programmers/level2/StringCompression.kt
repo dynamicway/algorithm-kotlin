2616be09c94d7dc34bfa7164d0bc29d3b8a81c20
bee <rkddbtjd123@gmail.com>
2022-03-03 01:00:37 +0900

    문자열 압축 구현

--- src/main/kotlin/programmers/level2/StringCompression.kt
+++ src/main/kotlin/programmers/level2/StringCompression.kt
@@ -1,5 +1,34 @@
 package programmers.level2
 
 fun stringCompression(s: String): Int {
-    return 0
+    if (s.length == 1) return 1
+    var result = Int.MAX_VALUE
+    (1..s.length / 2).forEach {
+        result = minOf(result, s.compressBy(it))
+    }
+    return result
+}
+
+private fun String.compressBy(number: Int): Int {
+    var result = Int.MAX_VALUE
+    val builder = StringBuilder()
+    var index = 0
+    var buffer = subSequence(0, index)
+    var count = 1
+    while (index + number <= length) {
+        val sequence = subSequence(index, index +  number).toString()
+        if (sequence == buffer) count++
+        else {
+            if (count == 1) builder.append(buffer)
+            else builder.append(count).append(buffer)
+            buffer = sequence
+            count = 1
+        }
+        index += number
+    }
+    if (count == 1) builder.append(buffer)
+    else builder.append(count).append(buffer)
+    builder.append(subSequence(index, length))
+    result = minOf(result, builder.length)
+    return result
 }
--- src/test/kotlin/programmers/level2/StringCompressionTest.kt
+++ src/test/kotlin/programmers/level2/StringCompressionTest.kt
@@ -6,5 +6,24 @@ import io.kotest.matchers.shouldBe
 
 class StringCompressionTest : FreeSpec({
 
+    "container" - {
+
+        "test01" {
+            stringCompression("aabbaccc") shouldBe 7
+        }
+
+        "test02" {
+            stringCompression("ababcdcdababcdcd") shouldBe 9
+        }
+
+        "test03" {
+            stringCompression("xababcdcdababcdcd") shouldBe 17
+        }
+
+        "test05" {
+            stringCompression("a") shouldBe 1
+        }
+
+    }
 
 })
