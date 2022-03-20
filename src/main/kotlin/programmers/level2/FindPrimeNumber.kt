48b474d85ba076be7c72dfd0b58252095ef0239d
bee <rkddbtjd123@gmail.com>
2022-03-21 02:15:49 +0900

    소수찾기 구현

--- src/main/kotlin/programmers/level2/FindPrimeNumber.kt
+++ src/main/kotlin/programmers/level2/FindPrimeNumber.kt
@@ -1,3 +1,96 @@
 package programmers.level2
 
-fun findPrimeNumber(number: String): Int = 0
+import kotlin.math.sqrt
+
+fun findPrimeNumber(number: String): Int {
+    val numbers = number.toCharArray().map { it - '0' }
+
+    val nodes = ArrayList<Node>()
+    numbers.toHashSet().forEach {
+        val node = Node("$it")
+        node.createChildren(numbers)
+        nodes.add(node)
+    }
+
+    val primeNumbers = HashSet<Int>()
+
+    nodes.forEach {
+        if (it.number.isPrimeNumber())
+            primeNumbers.add(it.number.toInt())
+        it.getPrimeNumbers(primeNumbers)
+    }
+
+    return primeNumbers.size
+}
+
+private class Node(
+    val number: String,
+    private val children: HashSet<Node> = HashSet()
+) {
+
+    fun createChildren(src: List<Int>) {
+        val toMutableList = src.toMutableList()
+
+        number.forEach {
+            toMutableList.remove(it - '0')
+        }
+
+        if (toMutableList.isEmpty())
+            return
+
+        toMutableList.forEach {
+            children.add(Node("$number$it"))
+        }
+
+        children.forEach {
+            it.createChildren(src)
+        }
+    }
+
+    fun getPrimeNumbers(primeNumbers: MutableSet<Int>) {
+        if (children.isEmpty())
+            return
+        children.forEach { child ->
+            if (child.number.isPrimeNumber())
+                primeNumbers.add(child.number.toInt())
+        }
+        children.forEach {
+            it.getPrimeNumbers(primeNumbers)
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Node
+
+        if (number != other.number) return false
+        if (children != other.children) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = number.hashCode()
+        result = 31 * result + children.hashCode()
+        return result
+    }
+
+
+}
+
+private fun String.isPrimeNumber(): Boolean {
+    val int = this.toInt()
+
+    when (int) {
+        0 -> return false
+        1 -> return false
+    }
+
+    (2..sqrt(int.toFloat()).toInt()).forEach {
+        if (int % it == 0) return false
+    }
+
+    return true
+}
--- src/test/kotlin/programmers/level2/FindPrimeNumbersTest.kt
+++ src/test/kotlin/programmers/level2/FindPrimeNumbersTest.kt
@@ -1,13 +1,43 @@
 package programmers.level2
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import kotlin.math.sqrt
 
 internal class FindPrimeNumbersTest: FreeSpec({
 
     "container" - {
 
         "test01" {
-            findPrimeNumber("asdf")
+            findPrimeNumber("17") shouldBe 3
+        }
+
+        "test02" {
+            findPrimeNumber("011") shouldBe 2
+        }
+
+        "test03" {
+            findPrimeNumber("0000") shouldBe 0
+        }
+
+        "test04" {
+            findPrimeNumber("00000001") shouldBe 0
+        }
+
+        "test05" {
+            findPrimeNumber("000002") shouldBe 1
+        }
+
+        "test06" {
+            findPrimeNumber("22222") shouldBe 1
+        }
+
+        "test07" {
+            findPrimeNumber("1231") shouldBe 18
+        }
+
+        "test08" {
+            println(sqrt(10.toFloat()).toInt())
         }
 
     }
