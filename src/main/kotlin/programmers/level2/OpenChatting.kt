119bb1cf9a8b14d6fd63f741a8d63534d882ba6a
bee <rkddbtjd123@gmail.com>
2022-03-24 23:32:28 +0900

    오픈채팅 구현

--- src/main/kotlin/programmers/level2/OpenChatting.kt
+++ src/main/kotlin/programmers/level2/OpenChatting.kt
@@ -1,5 +1,25 @@
 package programmers.level2
 
-fun openChatting(record: Array<String>): Array<String> {
-    return arrayOf()
+fun openChatting(records: Array<String>): Array<String> {
+    val lastChange = HashMap<String, String>()
+
+    records.forEach { record ->
+        if(!record.startsWith("L")) {
+            val splitRecord = record.split(" ")
+            lastChange[splitRecord[1]] = splitRecord[2]
+        }
+    }
+
+    val stringBuilder = StringBuilder()
+    val result = ArrayList<String>(records.size)
+
+    records.filter { !it.startsWith("C") }
+        .forEach {
+            val record = it.split(" ")
+            stringBuilder.append(lastChange[record[1]]).append(if (it.startsWith("E")) "님이 들어왔습니다." else "님이 나갔습니다.")
+            result.add(stringBuilder.toString())
+            stringBuilder.clear()
+        }
+
+    return result.toTypedArray()
 }
