0967478230734b21130e3d9bd736a52e381b7926
bee <rkddbtjd123@gmail.com>
2022-04-01 14:56:17 +0900

    N으로 표현 구현

--- src/test/kotlin/programmers/level3/ExpressedAsNTest.kt
+++ src/test/kotlin/programmers/level3/ExpressedAsNTest.kt
@@ -1,9 +1,28 @@
 package programmers.level3
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class ExpressedAsNTest : FreeSpec({
 
+    "container" - {
 
+        "test01" {
+            expressedAsN(5, 12) shouldBe 4
+        }
+
+        "test02" {
+            expressedAsN(2, 11) shouldBe 3
+        }
+
+        "test03" {
+            expressedAsN(9, 9) shouldBe 1
+        }
+
+        "test04" {
+            expressedAsN(2, 22) shouldBe 2
+        }
+
+    }
 
 })
