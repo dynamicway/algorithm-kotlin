616f4e4b9a53b959be3074292d8ff1de29290e0c
bee <rkddbtjd123@gmail.com>
2022-04-01 10:51:42 +0900

    N으로 표현 초기화

---
+++ src/main/kotlin/programmers/level3/ExpressedAsN.kt
@@ -0,0 +1,6 @@
+package programmers.level3
+
+fun expressedAsN(n: Int, number: Int): Int {
+
+    return 0
+}
---
+++ src/test/kotlin/programmers/level3/ExpressedAsNTest.kt
@@ -0,0 +1,9 @@
+package programmers.level3
+
+import io.kotest.core.spec.style.FreeSpec
+
+class ExpressedAsNTest : FreeSpec({
+
+
+
+})
