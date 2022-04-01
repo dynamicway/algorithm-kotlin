3c1f3e576bd8d65ce94a4536007d808ae0c25792
bee <rkddbtjd123@gmail.com>
2022-04-01 15:47:52 +0900

    입국심사 초기화

--- src/main/kotlin/programmers/level3/Immigration.kt
+++ src/main/kotlin/programmers/level3/Immigration.kt
@@ -1,2 +1,5 @@
 package programmers.level3
 
+fun immigration(n: Int, times: IntArray): Long {
+    return 0
+}
---
+++ src/test/kotlin/programmers/level3/ImmigrationTest.kt
@@ -0,0 +1,8 @@
+package programmers.level3
+
+import io.kotest.core.spec.style.FreeSpec
+
+class ImmigrationTest : FreeSpec({
+
+
+})
