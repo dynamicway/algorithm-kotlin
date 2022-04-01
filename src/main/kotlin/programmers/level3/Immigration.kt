9e28226bcbd080cee1281caba0cba8fbc372f3ac
bee <rkddbtjd123@gmail.com>
2022-04-01 17:26:12 +0900

    입국심사 구현

--- src/main/kotlin/programmers/level3/Immigration.kt
+++ src/main/kotlin/programmers/level3/Immigration.kt
@@ -1,5 +1,25 @@
 package programmers.level3
 
 fun immigration(n: Int, times: IntArray): Long {
-    return 0
+
+    var result = 0L
+    val officers = times.sorted()
+    var left = 0L
+    var right = times.last().toLong() * n
+
+    while (left <= right) {
+        val mid = (left + right) / 2
+        var sum = 0L
+        officers.forEach {
+            sum += mid / it
+        }
+        if (sum >= n) {
+            right = mid - 1
+            result = mid
+        }
+        else
+            left = mid + 1
+    }
+
+    return result
 }
--- src/test/kotlin/programmers/level3/ImmigrationTest.kt
+++ src/test/kotlin/programmers/level3/ImmigrationTest.kt
@@ -1,8 +1,17 @@
 package programmers.level3
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class ImmigrationTest : FreeSpec({
+
+    "container" - {
+
+        "test01" {
+            immigration(6, intArrayOf(7, 10)) shouldBe 28
+        }
+
+    }
 
 
 })
