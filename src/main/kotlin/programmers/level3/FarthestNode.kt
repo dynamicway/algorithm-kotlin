05b00f1613b503e41df8d4635d429c42c35a0f79
bee <rkddbtjd123@gmail.com>
2022-04-04 12:37:03 +0900

    가장 먼 노드 구현

--- src/main/kotlin/programmers/level3/FarthestNode.kt
+++ src/main/kotlin/programmers/level3/FarthestNode.kt
@@ -1,5 +1,29 @@
 package programmers.level3
 
 fun farthestNode(n: Int, edge: Array<IntArray>): Int {
-    return 0
+val adjacencyList = hashMapOf<Int, MutableList<Int>>()
+edge.forEach {
+    adjacencyList.computeIfAbsent(it[0]) { mutableListOf() }.add(it[1])
+    adjacencyList.computeIfAbsent(it[1]) { mutableListOf() }.add(it[0])
+}
+val nodes = hashMapOf<Int, MutableList<Int>>()
+nodes[1] = adjacencyList[1] ?: return 0
+
+val visited = BooleanArray(adjacencyList.size + 1)
+visited[1] = true
+adjacencyList[1]!!.forEach { visited[it] = true }
+
+var depth = 1
+while (true) {
+    val nextDepth = depth + 1
+    nodes[depth]!!.forEach {
+        val notVisitedNodes = adjacencyList[it]!!.filter { node -> !visited[node] }
+        notVisitedNodes.forEach { node -> visited[node] = true }
+        nodes.computeIfAbsent(nextDepth) { mutableListOf() }.addAll(notVisitedNodes)
+    }
+    if (nodes[depth]!!.size == 0) break
+    depth = nextDepth
+}
+
+return nodes[--depth]!!.size
 }
--- src/test/kotlin/programmers/level3/FarthestNodeTest.kt
+++ src/test/kotlin/programmers/level3/FarthestNodeTest.kt
@@ -1,9 +1,52 @@
 package programmers.level3
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class FarthestNodeTest : FreeSpec({
 
+    "container" - {
 
+        "test01" {
+            farthestNode(6, arrayOf(
+                intArrayOf(3, 6),
+                intArrayOf(4, 3),
+                intArrayOf(3, 2),
+                intArrayOf(1, 3),
+                intArrayOf(1, 2),
+                intArrayOf(2, 4),
+                intArrayOf(5, 2)
+            )) shouldBe 3
+        }
+
+        "test02" {
+            farthestNode(6, arrayOf(
+                intArrayOf(3, 6),
+                intArrayOf(4, 3),
+                intArrayOf(3, 2),
+                intArrayOf(2, 4),
+                intArrayOf(5, 2)
+            )) shouldBe 0
+        }
+
+        "test03" {
+            farthestNode(8, arrayOf(
+                intArrayOf(1, 2),
+                intArrayOf(1, 3)
+            )) shouldBe 2
+        }
+
+        "test04" {
+            farthestNode(8, arrayOf(
+                intArrayOf(1, 2),
+                intArrayOf(1, 3),
+                intArrayOf(3, 4),
+                intArrayOf(4, 5),
+                intArrayOf(5, 6),
+                intArrayOf(6, 7)
+            )) shouldBe 1
+        }
+
+    }
 
 })
