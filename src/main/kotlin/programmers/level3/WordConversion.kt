7afd8ef3fd24a123642b19a3d646ef31d1200cdf
bee <rkddbtjd123@gmail.com>
2022-04-06 21:43:14 +0900

    단어변환 초기화

---
+++ src/main/kotlin/programmers/level3/WordConversion.kt
@@ -0,0 +1,5 @@
+package programmers.level3
+
+fun wordConversion(begin: String, target: String, words: Array<String>): Int {
+    return 0
+}
---
+++ src/test/kotlin/programmers/level3/WordConversionTest.kt
@@ -0,0 +1,20 @@
+package programmers.level3
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class WordConversionTest : FreeSpec({
+
+    "container" - {
+
+        "test01" {
+            wordConversion(
+                "hit",
+                "cog",
+                arrayOf("hot", "dot", "dog", "lot", "log", "cog")
+            ) shouldBe 4
+        }
+
+    }
+
+})
