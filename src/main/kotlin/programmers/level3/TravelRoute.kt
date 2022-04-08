126d4db4019cba9037afa3801fb0ddde26cce664
bee <rkddbtjd123@gmail.com>
2022-04-08 17:42:24 +0900

    여행 루트 초기화

---
+++ src/main/kotlin/programmers/level3/TravelRoute.kt
@@ -0,0 +1,5 @@
+package programmers.level3
+
+fun travelRoute(tickets: Array<Array<String>>): Array<String> {
+    return arrayOf()
+}
---
+++ src/test/kotlin/programmers/level3/TravelRouteTest.kt
@@ -0,0 +1,11 @@
+package programmers.level3
+
+import io.kotest.core.spec.style.FreeSpec
+
+class TravelRouteTest : FreeSpec({
+
+    "container" - {
+
+    }
+
+})
