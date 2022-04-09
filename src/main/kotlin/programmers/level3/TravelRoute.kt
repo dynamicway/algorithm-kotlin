1e422aaebd6a6fac4911bd6dab9c0f14cafc23f5
bee <rkddbtjd123@gmail.com>
2022-04-09 13:49:46 +0900

    여행 경로 구현

--- src/main/kotlin/programmers/level3/TravelRoute.kt
+++ src/main/kotlin/programmers/level3/TravelRoute.kt
@@ -1,5 +1,30 @@
 package programmers.level3
 
+import java.util.*
+import kotlin.collections.ArrayDeque
+import kotlin.collections.HashMap
+
+private const val ICN = "ICN"
 fun travelRoute(tickets: Array<Array<String>>): Array<String> {
-    return arrayOf()
+
+    val adjacencyList = tickets.groupBy { it[0] }
+        .map { it.key to ArrayDeque(it.value.map { it[1] }.sorted()) }
+        .toMap() as HashMap
+
+    val result = mutableListOf<String>()
+
+    val stack = Stack<String>()
+    stack.push(ICN)
+
+    while (stack.isNotEmpty()) {
+        val peek = stack.peek()
+
+        if (!adjacencyList.containsKey(peek) || adjacencyList[peek]!!.isEmpty()) {
+            result.add(stack.pop())
+        } else {
+            stack.push(adjacencyList[peek]!!.removeFirst())
+        }
+    }
+
+    return result.reversed().toTypedArray()
 }
--- src/test/kotlin/programmers/level3/TravelRouteTest.kt
+++ src/test/kotlin/programmers/level3/TravelRouteTest.kt
@@ -1,10 +1,71 @@
 package programmers.level3
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class TravelRouteTest : FreeSpec({
 
     "container" - {
+
+        "test01" {
+            travelRoute(
+                arrayOf(
+                    arrayOf("ICN", "JFK"),
+                    arrayOf("HND", "IAD"),
+                    arrayOf("JFK", "HND"),
+                )
+            ) shouldBe arrayOf("ICN", "JFK", "HND", "IAD")
+        }
+
+        "test2" {
+            travelRoute(
+                arrayOf(
+                    arrayOf("JFK", "ICN"),
+                    arrayOf("ICN", "ATL"),
+                    arrayOf("ICN", "JFK"),
+                )
+            ) shouldBe arrayOf("ICN", "JFK", "ICN", "ATL")
+        }
+
+        "test3" {
+            travelRoute(
+                arrayOf(
+                    arrayOf("ICN", "NCI"),
+                    arrayOf("ICN", "NCI"),
+                    arrayOf("NCI", "ICN"),
+                    arrayOf("NCI", "NYC"),
+                )
+            ) shouldBe arrayOf("ICN", "NCI", "ICN", "NCI", "NYC")
+        }
+
+        "test04" {
+            travelRoute(
+                arrayOf(
+                    arrayOf("ICN", "AOO"),
+                    arrayOf("AOO", "BOO"),
+                    arrayOf("BOO", "COO"),
+                    arrayOf("COO", "DOO"),
+                    arrayOf("DOO", "EOO"),
+                    arrayOf("EOO", "DOO"),
+                    arrayOf("DOO", "COO"),
+                    arrayOf("COO", "BOO"),
+                    arrayOf("BOO", "AOO")
+                )
+            ) shouldBe arrayOf("ICN", "AOO", "BOO", "COO", "DOO", "EOO", "DOO", "COO", "BOO", "AOO")
+        }
+
+
+        "test05" {
+            travelRoute(
+                arrayOf(
+                    arrayOf("ICN", "AAA"),
+                    arrayOf("AAA", "BBB"),
+                    arrayOf("BBB", "AAA"),
+                    arrayOf("AAA", "ICN"),
+                    arrayOf("ICN", "AAA")
+                )
+            ) shouldBe arrayOf("ICN", "AAA", "BBB", "AAA", "ICN", "AAA")
+        }
 
     }
 
