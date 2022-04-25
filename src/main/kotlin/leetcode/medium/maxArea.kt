8a76d49b0de2b15ba50d43f55e68229361f25674
bee <rkddbtjd123@gmail.com>
2022-04-26 00:44:52 +0900

    maxArea 리팩토링

--- src/main/kotlin/leetcode/medium/maxArea.kt
+++ src/main/kotlin/leetcode/medium/maxArea.kt
@@ -1,23 +1,24 @@
 package leetcode.medium
 
-import java.lang.Integer.min
 import kotlin.math.max
+import kotlin.math.min
 
 fun maxArea(height: IntArray): Int {
-    var answer = 0
+
+    var result = -1
+
     var left = 0
-    var right = height.size - 1
+    var right = height.lastIndex
 
-    while (right - left > 0) {
-        val calcHeight = min(height[left], height[right])
-        val calcWidth = right - left
-        answer = max(answer, calcHeight * calcWidth)
-
-        if (height[left] > height[right]) {
-            right -= 1
-        } else {
-            left += 1
-        }
+    while (left <= right) {
+        val h = min(height[left], height[right])
+        val area = (right - left) * h
+        result = max(result, area)
+        if (h == height[left])
+            left++
+        else
+            right--
     }
-    return answer
+
+    return result
 }
