e80dcdaace40455dd39f3f861a5a85cc63b4b7ee
bee <rkddbtjd123@gmail.com>
2022-06-26 01:51:00 +0900

    다익스트라 선형탐색 구현

---
+++ src/main/kotlin/Dijkstra.kt
@@ -0,0 +1,40 @@
+object Dijkstra {
+
+    fun run(
+        nodes: Array<Array<Int?>>,
+        route: Pair<Int, Int>
+    ): Int? {
+        val start = route.first
+        val end = route.second
+        val visited = Array(nodes.size) { false }
+        val distances = Array<Int?>(nodes.size) { null }
+        distances[start] = 0
+        var currentNode = start
+
+        while (true) {
+            // 현재 노드를 방문처리한다.
+            visited[currentNode] = true
+            val notVisitedAdjacencyNodes = nodes[currentNode].mapIndexedNotNull { node, distance ->
+                if (distance == null || visited[node]) null
+                else node
+            }
+            // 모두 방문했거나 방문하지 않은 노드에서 다른 노드로 방문할 수 없는 경우 종료한다.
+            if (notVisitedAdjacencyNodes.isEmpty()) break
+            notVisitedAdjacencyNodes.forEach {
+                if (distances[it] != null) {
+                    distances[it] = minOf(distances[it]!!, distances[currentNode]!! + nodes[currentNode][it]!!)
+                } else distances[it] = distances[currentNode]!! + nodes[currentNode][it]!!
+            }
+            if (visited.none { !it }) break
+            val minDistance = distances.filterIndexed { node, distance -> !visited[node] && distance != null }
+                .minBy { it!! }
+            distances.forEachIndexed { node, distance ->
+                if (!visited[node] && distance == minDistance)
+                    currentNode = node
+            }
+        }
+
+        return distances[end]
+    }
+
+}
---
+++ src/test/kotlin/DijkstraTest.kt
@@ -0,0 +1,39 @@
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+internal class DijkstraTest: FreeSpec({
+
+    "linearSearch" - {
+
+        "test01" {
+
+            val nodes = arrayOf(
+                arrayOf(0, 2, null, 1, null, null),
+                arrayOf(2, 0, 3, 2, null, null),
+                arrayOf(null, 3, 0, null, null, 5),
+                arrayOf(1, 2, null, 0, 1, null),
+                arrayOf(null, null, null, 1, 0, 2),
+                arrayOf(null, null, 5, null, 2, 0)
+            )
+
+            Dijkstra.run(nodes, Pair(0, 5)) shouldBe 4
+
+        }
+
+        "test02" {
+
+            val nodes = arrayOf(
+                arrayOf(0, 2, 5, 1, null, null),
+                arrayOf(2, 0, 3, 2, null, null),
+                arrayOf<Int?>(5, 3, 0, 3, 1, 5),
+                arrayOf(1, 2, 3, 0, 1, null),
+                arrayOf(null, null, 1, 1, 0, 2),
+                arrayOf(null, null, 5, null, 2, 0)
+            )
+
+            Dijkstra.run(nodes, Pair(0, 5)) shouldBe 4
+
+        }
+    }
+
+})
